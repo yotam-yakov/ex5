78092291c849e7b171f2d7b4fa9be2b91a7e41d9
CSI-BIU <csi.biu2024@gmail.com>
2024-12-22 16:07:27 +0200

    Add files via upload

---
+++ ex5.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct Song {
+    
+} Song;
+
+typedef struct Playlist {
+   
+} Playlist;
+
+
+
+void deleteSong(Playlist* pl, int number) {
+
+    printf("Song deleted successfully.\n");
+}
+
+
+
+void playSong(Song* s) {
+    
+}
+
+
+
+void freeSong(Song* s) {
+
+}
+
+void freePlaylist(Playlist* pl) {
+    
+}
+
+void printPlaylistsMenu() {
+    printf("Please Choose:\n"); 
+    printf("\t1. Watch playlists\n\t2. Add playlist\n\t3. Remove playlist\n\t4. exit\n");   
+}
+
+
+
+
+
+
+
+
+void sortPlaylist(Playlist* pl) {
+    
+    printf("sorted\n");
+}
+
+
+
+int main() {
+    printf("Goodbye!\n");  
+}
